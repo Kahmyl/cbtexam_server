c45bb7e39800d1b798e22aa6cd4e96d3daa2e207
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2021-11-27 16:31:21 +0100

    .env

--- cbtexam/__init__.py
+++ cbtexam/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+<<<<<<< HEAD
+from cbtexam.config import Config
+=======
 
 from cbtexam.users.routes import users
 from .config import Config
+>>>>>>> 758cb92f8ff2662099ae015a0e02fb903833f462
 
+db = SQLAlchemy()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
+
+    db.init_app(app)
 
 
     from cbtexam.users.routes import users
@@ -17,4 +25,3 @@ def create_app(config_class=Config):
 
 
     return app
-
--- cbtexam/users/routes.py
+++ cbtexam/users/routes.py
@@ -1,8 +1,33 @@
-
 from flask import Blueprint, render_template
+from flask_bcrypt import Bcrypt
+from flask import request, jsonify
+from cbtexam import db
+from cbtexam.users.models import UsersModel
+from flask_login import login_user, current_user, logout_user, login_required
 
 users = Blueprint('users', __name__)
+bcrypt = Bcrypt()
 
 @users.route('/users')
 def hello():
     return "Hello"
+
+
+@users.route("/register", methods=['POST'])
+def register():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+            new_user = UsersModel(name=data['name'], email=data['email'], password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return {"message": f"user {new_user.name} has been registered successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+@users.route("/login", methods=['POST'])
+def login():
+    data = request.json
+    return data
+        
