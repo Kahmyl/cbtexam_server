25a551a961ba6bd407e7f0dc22a1b20d86c2d52e
moyosore1 <35351716+moyosore1@users.noreply.github.com>
2021-11-28 14:46:11 -0500

    added blacklist tokens

--- cbtexam/admin/models.py
+++ cbtexam/admin/models.py
@@ -30,8 +30,35 @@ class Admin(db.Model):
         # Decodes auth token returns integer or string
         try:
             payload = jwt.decode(auth_token, os.environ.get('SECRET_KEY'))
-            return payload['sub']
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again.'
+
+
+class BlacklistToken(db.Model):
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token 
+        self.blacklisted_on = datetime.now()
+    
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether auth token has been blacklisted
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+        else:
+            return False
+
+    
--- cbtexam/admin/routes.py
+++ cbtexam/admin/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify, request
 from cbtexam import bcrypt, db
 
 
-from .models import Admin
+from .models import Admin, BlacklistToken
 
 admin = Blueprint('admin', __name__)
 
@@ -54,7 +54,14 @@ def login():
 def get_user():
     auth_header = request.headers.get('Authorization')
     if auth_header:
-        auth_token = auth_header.split(" ")[1]
+        try:
+            auth_token = auth_header.split(" ")[1]
+        except IndexError:
+            response_object = {
+                'status': 'fail',
+                'message': 'Bearer token malformed.'
+            }
+            return jsonify(response_object), 401
     else:
         auth_token = ''
     if auth_token:
@@ -81,3 +88,46 @@ def get_user():
             'message': 'Provide a valid auth token.'
         }
         return jsonify(response_object), 500
+
+
+@admin.route('/admin/logout', methods=['POST'])
+def admin_logout():
+    if request.method == "POST":
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+            print(auth_token)
+        else:
+            auth_token = ""
+
+        if auth_token:
+            resp = Admin.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                blacklist_token = BlacklistToken(token=auth_token)
+                try:
+                    db.session.add(blacklist_token)
+                    db.session.commit()
+                    response_object = {
+                        'status': 'success',
+                        'message': 'Successfully logged out'
+                    }
+                    return jsonify(response_object), 200
+                except Exception as e:
+                    response_object = {
+                        'status': 'failed',
+                        'message': e
+                    }
+                    return jsonify(response_object), 200
+            else:
+                response_object = {
+                    'status': 'failed',
+                    'message': resp
+                }
+                return jsonify(response_object), 401
+        else:
+            response_object = {
+                'status': 'failed',
+                'message': 'Provide a valid auth token'
+            }
+            return jsonify(response_object), 403
+
