93da550854541398d916aaae5e29844f7bdbc621
moyosore1 <35351716+moyosore1@users.noreply.github.com>
2021-11-28 14:11:37 -0500

    added jwt

--- cbtexam/__init__.py
+++ cbtexam/__init__.py
@@ -6,14 +6,17 @@ from flask_bcrypt import Bcrypt
 from .config import Config
 
 db = SQLAlchemy()
+
+
 bcrypt = Bcrypt()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
     db.init_app(app)
+
     bcrypt.init_app(app)
 
     from cbtexam.users.routes import users
@@ -21,5 +24,9 @@ def create_app(config_class=Config):
 
     app.register_blueprint(users)
     app.register_blueprint(admin)
+
+    from cbtexam.admin.models import Admin
+    # with app.app_context():
+    #     db.create_all()
 
     return app
--- cbtexam/admin/routes.py
+++ cbtexam/admin/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 
+from cbtexam import bcrypt, db
+
 
 from .models import Admin
 
@@ -8,7 +10,74 @@ admin = Blueprint('admin', __name__)
 
 @admin.route('/admin/register', methods=['GET', 'POST'])
 def create_admin():
-    if request.method == "POST":
-        pass
+    if request.method == "POST" and request.is_json:
+        data = request.get_json()
+        admin = Admin.query.filter_by(username=data.get('username')).first()
+        if not admin:
+            hashed_password = bcrypt.generate_password_hash(
+                data.get('password')).decode('utf-8')
+            admin = Admin(username=data.get('username'),
+                          password=hashed_password)
+            db.session.add(admin)
+            db.session.commit()
+            return jsonify({"message": "Admin account has been created successfully"})
+    else:
+        return jsonify({"error": "The request payload is not in JSON format"})
 
 
+@admin.route('/admin/login/', methods=['POST'])
+def login():
+    if request.method == "POST" and request.is_json:
+        data = request.get_json()
+        try:
+            admin = Admin.query.filter_by(
+                username=data.get('username')).first()
+            if admin and bcrypt.check_password_hash(admin.password, data.get('password')):
+                auth_token = admin.encode_auth_token(admin.id)
+
+                if auth_token:
+                    response_object = {
+                        'status': 'success',
+                        'message': 'Successfully logged in.',
+                        'auth_token': auth_token.decode()
+                    }
+                    return jsonify(response_object), 200
+
+        except Exception as e:
+            response_object = {
+                'status': 'fail',
+                'message': 'Some error occurred. Please try again.'
+            }
+            return jsonify(response_object), 500
+
+
+def get_user():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        auth_token = ''
+    if auth_token:
+        resp = Admin.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            admin = Admin.query.filter_by(id=resp).first()
+            response_object = {
+                'status': 'success',
+                'data': {
+                    'user_id': admin.id,
+                    'username': admin.username,
+                }
+            }
+            return jsonify(response_object), 200
+
+        response_object = {
+            'status': 'fail',
+            'message': resp
+        }
+        return jsonify(response_object), 500
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'Provide a valid auth token.'
+        }
+        return jsonify(response_object), 500
